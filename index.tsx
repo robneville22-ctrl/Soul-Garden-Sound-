24c60800238f66d51777d0cadb29672587d3949e
Rob Neville <rob.neville22@gmail.com>
2026-01-02 20:32:06 -0600

    Remove outdated pnpm-lock.yaml to fix build - will be regenerated

--- index.tsx
+++ index.tsx
@@ -1,10 +1,3 @@
-// Polyfill Buffer for gray-matter in browser
-import { Buffer } from 'buffer';
-if (typeof window !== 'undefined') {
-  (window as any).Buffer = Buffer;
-  (globalThis as any).Buffer = Buffer;
-}
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
